1842dad803a55ba43e4899059ade3ad8e83657b1
prasadc01 <prasadc@edreamz.in>
2022-10-14 11:07:37 +0530

    init commit

---
+++ src/admin/app.example.js
@@ -0,0 +1,43 @@
+export default {
+  config: {
+    auth: {
+      logo:'https://res.cloudinary.com/usability-designs/image/upload/v1659788800/villazzo_logo_83ad6b8fc4.png?w=1920&q=75',
+    },
+    menu:{
+      logo: 'https://res.cloudinary.com/usability-designs/image/upload/v1659788800/villazzo_logo_83ad6b8fc4.png?w=1920&q=75'
+    },
+    tutorials: false,
+    notifications: { release: false },
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
---
+++ src/admin/app.js
@@ -0,0 +1,49 @@
+export default {
+    config: {
+      auth: {
+        logo:'https://res.cloudinary.com/usability-designs/image/upload/v1659788800/villazzo_logo_83ad6b8fc4.png?w=1920&q=75',
+        title:'Villazzo',
+        name:'Villazzo'
+      },
+      menu:{
+        logo: 'https://res.cloudinary.com/usability-designs/image/upload/v1660062424/villazzo-icon_zaviwg.jpg'
+      },
+      head: {
+        favicon: 'https://res.cloudinary.com/usability-designs/image/upload/v1659788396/favicon_81303dc265.png',
+      },
+      tutorials: false,
+      notifications: { release: false },
+      locales: [
+        // 'ar',
+        // 'fr',
+        // 'cs',
+        // 'de',
+        // 'dk',
+        // 'es',
+        // 'he',
+        // 'id',
+        // 'it',
+        // 'ja',
+        // 'ko',
+        // 'ms',
+        // 'nl',
+        // 'no',
+        // 'pl',
+        // 'pt-BR',
+        // 'pt',
+        // 'ru',
+        // 'sk',
+        // 'sv',
+        // 'th',
+        // 'tr',
+        // 'uk',
+        // 'vi',
+        // 'zh-Hans',
+        // 'zh',
+      ],
+    },
+    bootstrap(app) {
+      console.log(app);
+    },
+  };
+  
